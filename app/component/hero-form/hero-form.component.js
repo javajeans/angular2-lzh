4027df3f8c09ebe1f53f025f87d67a27cf0eb12b
zhiheng.li <zhiheng.li@qishon.com>
2016-11-01 17:52:42 +0800

    1.完成的表单DEMO

--- app/component/hero-form/hero-form.component.js
+++ app/component/hero-form/hero-form.component.js
@@ -19,8 +19,15 @@ var HeroFormComponent = (function () {
             'Super Hot', 'Weather Changer'];
         this.model = new HeroForm_1.HeroForm(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
         this.submitted = false;
+        this.active = true;
     }
     HeroFormComponent.prototype.onSubmit = function () { this.submitted = true; };
+    HeroFormComponent.prototype.newHero = function () {
+        var _this = this;
+        this.model = new HeroForm_1.HeroForm(42, '', '');
+        this.active = false;
+        setTimeout(function () { return _this.active = true; }, 0);
+    };
     Object.defineProperty(HeroFormComponent.prototype, "diagnostic", {
         // TODO: Remove this when we're done
         get: function () { return JSON.stringify(this.model); } //我们在最后增加一个 diagnostic 属性，它返回这个模型的 JSON 形式。 它会帮我们看清开发过程中发生的事，等最后做清理时我们会丢弃它。
@@ -32,7 +39,8 @@ var HeroFormComponent = (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'hero-form',
-            templateUrl: 'hero-form.component.html'
+            templateUrl: 'hero-form.component.html',
+            styleUrls: ['hero-form.component.css']
         }), 
         __metadata('design:paramtypes', [])
     ], HeroFormComponent);
